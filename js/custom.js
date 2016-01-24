11308a44c7044c34f49bf6c80f3161a6f2b3970d
Muthu Palaniappan <amuthu1996@gmail.com>
2016-01-24 21:15:47 +0530

    semi-complete

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,10 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+$(function eventModal(arg) {
+	var addr = "http://ptpb.pe.hu/api/event/" + arg;
+	$.get( addr, function( data ) {
+	  $( "#project-1-label" ).html( data.name );
+	  
+	});
+});
